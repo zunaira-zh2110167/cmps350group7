4c1465ffdd5d2b5d1d7a52f9b58a04d39c75bd45
yara-yk2104889 <yarah24.k@gmail.com>
2025-05-15 16:15:46 +0300

    update

--- mizan/app/_repo/CommentRepo.js
+++ mizan/app/_repo/CommentRepo.js
@@ -1,7 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/lib/prisma";
 import userRepo from "./UserRepo";
-
-const prisma = new PrismaClient();
 
 class CommentRepo {
   async getComments(sectionCRN) {

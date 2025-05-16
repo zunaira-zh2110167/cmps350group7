c3bbdbaa4be0306b4d887c199d6a7da9796ad216
sara-sa2002331 <158162381+sara-sa2002331@users.noreply.github.com>
2025-05-16 17:50:21 +0300

    Update for Header

--- mizan/app/(main)/components/Header.jsx
+++ mizan/app/(main)/components/Header.jsx
@@ -1,13 +1,51 @@
-import styles from "./Header.module.css";
+import Link from "next/link";
 import { getCurrentUser } from "@/app/actions/auth";
+import LogoutButton from "./LogoutButton";
+import styles from "./Header.module.css";
 import ClientNavLinks from "./ClientNavLinks";
+const links = [
+  {
+    label: "Assessments",
+    route: "/assessments",
+  },
+  {
+    label: "Workload Report",
+    route: "/workload-report",
+  },
+];
 
 export default async function Header() {
+  // Get current user (if logged in)
   const user = await getCurrentUser();
+
   return (
     <header className={styles.header}>
       <div className={styles.brandLogo}>Mizān</div>
       <ClientNavLinks user={user} />
+      <nav className={styles.navContainer}>
+        <ul className={styles.navigation}>
+          {links.map(({ label, route }) => (
+            <li className={styles.navigationItem} key={route}>
+              <Link href={route}>{label}</Link>
+            </li>
+          ))}
+        </ul>
+
+        <div className={styles.authSection}>
+          {user ? (
+            <>
+              <span className={styles.userName}>
+                🤗 Welcome {user.firstName} {user.lastName}
+              </span>
+              (<LogoutButton />)
+            </>
+          ) : (
+            <Link href="/login" className={styles.loginLink}>
+              Login
+            </Link>
+          )}
+        </div>
+      </nav>
     </header>
   );
 }
